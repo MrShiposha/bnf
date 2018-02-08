29ca1fccf3ede86788d89277219e05e25b0cf0b9
Shea Newton <sheanewt@gmail.com>
2018-02-07 18:36:05 -0800

    trying out an earley predictor using a HashSet of tuples

--- example/src/main.rs
+++ example/src/main.rs
@@ -11,30 +11,38 @@ use std::collections::HashSet;
 // let input =
 // r#"
 // <Rule1> ::= <Rule2> | <Rule2> <Rule1>
-// <Rule2> ::= "ABC" | "AB" | "BC" | "AC" | "AG" | "T" | <Rule3> | <Rule4>
-// <Rule3> ::= "AB" | "BC" | "AC" | "AG" | "T" | <Rule4>
+// <Rule2> ::= "ABC" | "AB" | "BC" | "AC" <Rule3> | <Rule4>
+// <Rule3> ::= "AB" | "BC" | "AG" | "T" | <Rule4>
 // <Rule4> ::= "BC" | "AC"
 // <Rule5> ::= "QR" | "ST"
 // "#;
 
-fn earley_predictor(grammar: &Grammar, term: &Term) -> HashSet<Expression> {
-    let mut candidates: HashSet<Expression> =  HashSet::new();
+fn earley_predictor(grammar: &Grammar, term: &Term) -> HashSet<(Term, Expression)> {
+    let mut candidates: HashSet<(Term, Expression)> = HashSet::new();
     for prod in grammar.productions_iter() {
         if prod.lhs == *term {
             for expr in prod.rhs_iter() {
-                candidates.insert(expr.clone());
+                candidates.insert((prod.lhs.clone(), expr.clone()));
             }
         }
     }
 
-    candidates
+    let mut container: HashSet<(Term, Expression)> = HashSet::new();
+    for expr in &candidates {
+        for t in expr.1.terms_iter() {
+            if t != term {
+                container = earley_predictor(&grammar, t).union(&container).cloned().collect();
+            }
+        }
+    }
+
+    candidates.union(&container).cloned().collect()
 }
 
 fn earley_scanner(grammar: &Grammar, term: &Term) -> HashSet<Term> {
     let mut matches: HashSet<Term> =  HashSet::new();
     for prod in grammar.productions_iter() {
         for expr in prod.rhs_iter() {
-            expr_as_nonterm(expr.clone());
             for t in expr.terms_iter() {
                 if let Term::Terminal(_) = *t {
                     if t == term {
@@ -78,7 +86,7 @@ fn main() {
     println!("matches: {:?}", matches);
 
     let term = Term::Nonterminal(String::from("Rule1"));
-    let candidates: HashSet<Expression> = earley_predictor(&grammar, &term);
+    let candidates: HashSet<(Term, Expression)> = earley_predictor(&grammar, &term);
 
-    println!("candidates: {:?}", candidates);
+    println!("candidates: {:#?}", candidates);
 }
