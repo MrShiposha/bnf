bc293464d24f11ed4494034a7bc09d791f6b2576
Shea Newton <shnewto@gmail.com>
2018-02-21 18:54:23 -0800

    stash, incorporating state loop into example

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,42 +4,82 @@ use bnf::Grammar;
 use bnf::Term;
 use std::collections::HashSet;
 
-fn earley_predictor(grammar: &Grammar, term: &Term) -> HashSet<(Term, Vec<Term>)> {
-    let mut candidates: HashSet<(Term, Vec<Term>)> = HashSet::new();
-    for prod in grammar.productions_iter() {
-        if prod.lhs == *term {
-            for expr in prod.rhs_iter() {
-                candidates.insert((prod.lhs.clone(), expr.terms_iter()
-                                                         .cloned()
-                                                         .collect()));
-            }
-        }
-    }
-
-    let mut container: HashSet<(Term, Vec<Term>)> = HashSet::new();
-    for candidate in &candidates {
-        for c in &candidate.1 {
-            if let Term::Nonterminal(_) = *c {
-                if c != term {
-                    container = earley_predictor(&grammar, &c).union(&container)
-                                                              .cloned()
-                                                              .collect();
-                }
-            }
-        }
-    }
-
-    candidates.union(&container).cloned().collect()
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct PartsOfSpeech {
+    terms: HashSet<Term>,
 }
 
-fn earley_scanner(grammar: &Grammar, term: &Term) -> HashSet<Term> {
-    let mut matches: HashSet<Term> =  HashSet::new();
-    for prod in grammar.productions_iter() {
-        for expr in prod.rhs_iter() {
-            for t in expr.terms_iter() {
-                if let Term::Terminal(_) = *t {
-                    if t == term {
-                        matches.insert(prod.lhs.clone());
+impl PartsOfSpeech {
+    /// Construct a new `PartsOfSpeech`
+    pub fn new() -> PartsOfSpeech {
+        PartsOfSpeech { terms: HashSet::new() }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct State {
+    origin: Term,
+    rules: HashSet<Vec<Term>>,
+    dot: usize,
+}
+
+// impl State {
+//     /// Construct a new `State`
+//     pub fn new() -> State {
+//         State { rules: HashSet::new(), dot: 0 }
+//     }
+// }
+
+// #[derive(Clone, Debug, Eq, Hash, PartialEq)]
+// struct State {
+//     parts_of_speech: PartsOfSpeech,
+//     grammar_rules: State,
+// }
+
+// fn earley_predictor(grammar: &Grammar, term: &Term) -> State {
+//     let mut candidates = State::new();
+//     for prod in grammar.productions_iter() {
+//         if prod.lhs == *term {
+//             for expr in prod.rhs_iter() {
+//                 candidates.rules.insert((prod.lhs.clone(), expr.terms_iter()
+//                                                                .cloned()
+//                                                                .collect()));
+//             }
+//         }
+//     }
+
+//     let mut container: State = State::new();
+//     for candidate in &candidates.rules {
+//         for c in &candidate.1 {
+//             if let Term::Nonterminal(_) = *c {
+//                 if c != term {
+//                     container.rules =
+//                         earley_predictor(&grammar, &c)
+//                                                     .rules
+//                                                     .union(&container.rules)
+//                                                     .cloned()
+//                                                     .collect();
+//                 }
+//             }
+//         }
+//     }
+
+//     State{ rules: candidates.rules.union(&container.rules).cloned().collect(), dot: 0 }
+// }
+
+fn earley_scanner(term: &Term, k: usize, words: String, grammar: &Grammar) -> PartsOfSpeech {
+    let mut matches: PartsOfSpeech =  PartsOfSpeech::new();
+    let mut pattern: String = String::new();
+    for(_, c) in words[k..].chars().enumerate() {
+        pattern.push(c);
+        let new_term = Term::Terminal(pattern.clone());
+        for prod in grammar.productions_iter() {
+            for expr in prod.rhs_iter() {
+                for t in expr.terms_iter() {
+                    if let Term::Terminal(_) = *t {
+                        if t == term {
+                            matches.terms.insert(prod.lhs.clone());
+                        }
                     }
                 }
             }
@@ -47,6 +87,28 @@ fn earley_scanner(grammar: &Grammar, term: &Term) -> HashSet<Term> {
     }
 
     matches
+}
+
+fn earlt_init(grammar: &Grammar) -> Option<State> {
+    if let Some(prod) = grammar.productions_iter().nth(0) {
+        let origin = prod.lhs.clone();
+        let mut rules: HashSet<Vec<Term>> = HashSet::new();
+        for expr in prod.rhs_iter() {
+            rules.insert(expr.terms_iter().cloned().collect::<Vec<_>>());
+        }
+
+        return Some(State { origin, rules, dot: 0 })
+    }
+
+    return None
+}
+
+fn earley_finished(state: &State, rules: &Vec<Term> ) -> bool {
+    if state.dot == (rules.len()) {
+        return true
+    }
+
+    return false
 }
 
 fn main() {
@@ -61,26 +123,32 @@ fn main() {
     let grammar = Grammar::from_str(input).unwrap();
 
     // scanner
-    let input = String::from("1234");
-    let mut pattern: String = String::new();
-    let mut matches: HashSet<Term> =  HashSet::new();
+    let input = String::from("2 + 3 * 4");
+
+    let candidates: State;
+    let mut states: Vec<State> = vec![];
+
+    if let Some(state) = earlt_init(&grammar) {
+        states.push(state);
+    } else {
+        return
+    }
+
     for i in 0..input.len() {
-        for(_, c) in input[i..].chars().enumerate() {
-            pattern.push(c);
-            let new_term = Term::Terminal(pattern.clone());
-            matches = earley_scanner(&grammar, &new_term).union(&matches).cloned().collect();
+        for state in &mut states {
+            for rule in &state.rules {
+                if !earley_finished(&state, rule) {
+
+                    // let _ = earley_scanner(&grammar, &new_term).union(&matches)
+                    //                                             .cloned()
+                    //                                             .collect();
+
+                    // candidates = earley_predictor(&grammar, &term);
+                } else {
+                    //
+                }
+            }
         }
-        pattern.clear();
     }
 
-    // println!("matches: {:?}\n\n", matches);
-
-    let term = Term::Nonterminal(String::from("P"));
-    let candidates: HashSet<(Term, Vec<Term>)> = earley_predictor(&grammar, &term);
-
-    for candidate in candidates {
-        println!("{:?}", candidate);
-    }
-
-    // println!("candidates: {:#?}", candidates);
 }
